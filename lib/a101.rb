26598c805599e5b5c3103b02eef03bfb90767d74
Leonid Myravjev <asm@asm.pp.ru>
2018-10-30 23:20:21 +0300

    get info from new url (new site)

--- lib/a101.rb
+++ lib/a101.rb
@@ -5,6 +5,7 @@ require 'rails'
 require 'excon'
 require 'nokogiri'
 
+Excon.ssl_verify_peer = false
 Cache=ActiveSupport::Cache::FileStore.new("tmp")
 def GET(uri)
 	res=Cache.fetch(uri, :expires_in => 60) {
@@ -14,17 +15,21 @@ def GET(uri)
 	}
 	res 
 end
-body=GET("https://old.a101.ru/projects/moskva-a101/kupit/parkovochnyie-mesta")
+#body=GET("https://old.a101.ru/projects/moskva-a101/kupit/parkovochnyie-mesta") # old site
+body=GET("https://a101.ru/projects/moskva-a101/building/226/parking/#genplan")
 b=Nokogiri::HTML(body)
 
 hold=0
 free=0
 total=0
-b.css("div.park-place").each{|x|
-	if x['class'].to_s =~ /hold-place/
-		hold+=1
-	else
-		free+=1
+reserve=0
+b.css("g.parkplace").each{|x|
+  if x.to_s =~ /fill="rgba/
+    if x.to_s =~ /rgba\(230/
+          hold+=1
+    else  reserve+=1
+    end
+	else    free+=1
 	end
 	total+=1
 }
@@ -32,4 +37,5 @@ b.css("div.park-place").each{|x|
 printf "Статистика заполнения паркинга на основе сайта a101 от #{Time.new.strftime("%d.%m.%Y")} - %5.2f%%:\n", hold/total.to_f*100
 puts "Всего - #{total}"
 puts "Свободно - #{free}"
+puts "В резерве - #{reserve}"
 puts "Занято - #{hold}"
